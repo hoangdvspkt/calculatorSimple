5560ff539357a56cc485ac9cf89cb95718141167
hoangdv <royyhoang@hoangdvs-Mac.local>
2017-04-14 17:28:34 -0700

    Initial Commit

---
+++ Calculator_Simple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator_Simple
+//
+//  Created by hoangdv on 4/14/17.
+//  Copyright © 2017 com.hoangdv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
